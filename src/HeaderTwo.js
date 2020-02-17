3a4913447c0619702edff3e3c95e82a4e8602548
Michael Rodriguezls <miker@uchicago.edu>
2020-02-17 14:00:56 -0600

    added post image

--- src/HeaderTwo.js
+++ src/HeaderTwo.js
@@ -1,20 +1,47 @@
 import React from 'react';
-import headerImage from './headerTwo.jpg';
-import './App.css';
-import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
-const topicStyle = {
-  paddingRight:'4rem',
-  paddingLeft:'4rem',
-};
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '150%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+}));
+
+
 
 export default function HeaderTwo() {
-    var text = require('./text.json');
+  const classes = useStyles();
     return (
         <header className="App-header">
-            <img src={headerImage} className="App-logo" alt="logo" />
-            <div style={{padding: '1em'}}/>
-            <Typography variant="body1" style={topicStyle}>{text.HeaderTwo}</Typography>
+          <Card style={{marginTop: "10vh", width: "60vh", height: "90vh"}}>
+            <CardMedia  className={classes.media} title="poster" image={require('./headerTwo.jpg')}/>
+          </Card>
+          <Button
+                variant="contained"
+                color="default"
+                href="https://drive.google.com/open?id=1N1G1DBu3tMYbzQa54Q44DdyATRwPMBad"
+                className={classes.button}
+              >
+              Image Download
+              </Button>
         </header>
       );
     }
